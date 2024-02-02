34c89b0474e0460981b09c1cb7add92ce08cb319
OmarElsakka1 <s-omar.elsakka@zewailcity.edu.eg>
2024-02-02 21:30:08 +0000

    bst

---
+++ 113-bst_search.c
@@ -0,0 +1,25 @@
+#include "binary_trees.h"
+
+/**
+ * bst_search - Searching for a value in a binary search tree.
+ * @tree: A pointer to the root node of the BST to search.
+ * @value: The value to search for in the BST.
+ *
+ * Return: a pointer to the node containing the value, or even NULL.
+ */
+bst_t *bst_search(const bst_t *tree, int value)
+{
+if (tree != NULL)
+{
+if (tree->n == value)
+{
+return ((bst_t *)tree);
+}
+if (tree->n > value)
+{
+return (bst_search(tree->left, value));
+}
+return (bst_search(tree->right, value));
+}
+return (NULL);
+}
